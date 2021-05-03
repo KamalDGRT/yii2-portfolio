df1da6be5e72334ddaf1b260bca16db18581c1a8
KamalDGRT <kamaldgrt@gmail.com>
2021-05-03 08:33:43 +0530

    quickfix: add ThemeQuery

---
+++ common/models/query/ThemeQuery.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace common\models\query;
+
+/**
+ * This is the ActiveQuery class for [[\common\models\Theme]].
+ *
+ * @see \common\models\Theme
+ */
+class ThemeQuery extends \yii\db\ActiveQuery
+{
+    /*public function active()
+    {
+        return $this->andWhere('[[status]]=1');
+    }*/
+
+    /**
+     * {@inheritdoc}
+     * @return \common\models\Theme[]|array
+     */
+    public function all($db = null)
+    {
+        return parent::all($db);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return \common\models\Theme|array|null
+     */
+    public function one($db = null)
+    {
+        return parent::one($db);
+    }
+
+    public function creator($userId)
+    {
+        return $this->andWhere(['created_by' => $userId]);
+    }
+}
