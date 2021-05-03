48cb8a64f913b32616537401a91522578c87b698
KamalDGRT <kamaldgrt@gmail.com>
2021-05-03 23:08:51 +0530

    fix: [profilepic] basic approach works

--- frontend/views/profilepic/index.php
+++ frontend/views/profilepic/index.php
@@ -9,51 +9,56 @@ use yii\data\ActiveDataProvider;
 $this->title = 'Profile Picture';
 ?>
 
-<div style="max-width:450px;">
-    <div class="content">
 
-        <div class="jagya15bottom"></div>
-        <br><br><br>
-        <div class="center-text">
-            <h3>Profile Picture</h3>
-            <div class="jagya20bottom"></div>
-            <?php
-            $profiles = new ActiveDataProvider([
-                'query' => Profilepic::find()->where(['created_by' => Yii::$app->user->identity->id]),
-            ]);
-            $profileCount = $profiles->getTotalCount();
-            $hasProfilePic = $profileCount;
-            $imageDetail = Profilepic::findOne(['created_by' => Yii::$app->user->identity->id]);
-            // echo '<pre>' . var_dump($imageDetail) . '</pre>';
+<div class="content">
 
-            //echo $imageDetail->id;
-            if ($hasProfilePic == 0) {
+    <div class="jagya15bottom"></div>
+    <br><br><br>
+    <div class="center-text">
+
+        <div class="jagya20bottom"></div>
+        <?php
+        $profiles = new ActiveDataProvider([
+            'query' => Profilepic::find()->where(['created_by' => Yii::$app->user->identity->id]),
+        ]);
+        $profileCount = $profiles->getTotalCount();
+        $hasProfilePic = $profileCount;
+        $imageDetail = Profilepic::findOne(['created_by' => Yii::$app->user->identity->id]);
+        // echo '<pre>' . var_dump($imageDetail) . '</pre>';
+
+        if ($hasProfilePic == 0) {
+        ?>
+            <?= Html::a(
+                'Upload a Profile Photo',
+                ['/profilepic/create/'],
+                ['class' => 'btn btn-lg btn-danger col-sm-12'],
+            )
             ?>
-                <?= Html::a(
-                    'Upload a Profile Photo',
-                    ['/profilepic/create/'],
-                    ['class' => 'btn btn-lg btn-danger col-sm-12'],
-                )
-                ?>
-            <?php } else if ($hasProfilePic == 1) {  ?>
-                <?= Html::beginForm(['/profilepic/delete', 'id' => $imageDetail->id], 'post'); ?>
-                <?= Html::submitButton(
-                    'Remove Current Profile Photo & Add a New One',
-                    [
-                        'class' => 'btn btn-lg btn-danger col-sm-12',
-                    ],
-                )
-                ?>
-                <?= Html::endForm() ?>
-                <?= Html::a(
-                    'View Profile Photo',
-                    ['/profilepic/view/' . $imageDetail->id],
-                    ['class' => 'btn btn-lg btn-danger col-sm-12'],
-                )
-                ?>
-            <?php } ?>
-        </div>
+        <?php } else if ($hasProfilePic == 1) {  ?>
 
+            <h3 class="center-text">Hi!</h3>
+            <h4 class="center-text">This is your Profile Picture !</h4>
+
+            <?php
+
+            echo Html::img($imageDetail->getImageUrl(), [
+                'style' => 'margin: auto;',
+            ]);
+
+
+            // render the submit button
+            ?>
+            <?= Html::beginForm(['/profilepic/delete', 'id' => $imageDetail->id], 'post'); ?>
+            <?= Html::submitButton(
+                'Remove Current Profile Photo & Add a New One',
+                [
+                    'class' => 'btn btn-lg btn-danger col-sm-12',
+                ],
+            )
+            ?>
+            <?= Html::endForm() ?>
+
+        <?php } ?>
     </div>
 
 </div>
